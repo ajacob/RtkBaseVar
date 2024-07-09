3868fa7f03d48f12b5d12f681277b86c79dbdfb1
Alexandre Jacob <alexandref.jacob@gmail.com>
2024-07-09 22:44:25 +0200

    Minor changes in run.sh

--- pybasevar/run.sh
+++ pybasevar/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 echo "SYSTEM: create serial /dev/pts/1 /2"
-##TODO bug when socat start from pybasegnss.py, Temporarily it starts here
 socat -d -d pty,raw,echo=0 pty,raw,echo=0 &>/dev/null &
-sleep 3 &&
+sleep 3
 echo "SYSTEM: Start pybasevar"
-echo "SYSTEM: Telegram connexion > apikey :"$APIKEY " / user id" $USERID
+echo "SYSTEM: Telegram connexion > [APIKEY: $APIKEY | USERID: $USERID]"
 python3 /home/pybasevar.py
